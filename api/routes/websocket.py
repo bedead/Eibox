b3591d6acf4e738387d16a49e79fc41d0a0a7c8a
Satyam Mishra <satyammishra9050@gmail.com>
2025-06-29 20:02:59 +0530

    feat(api): add api mainAgent websocket endpoint

--- api/routes/websocket.py
+++ api/routes/websocket.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, WebSocket
 from langgraph.config import RunnableConfig
 from langgraph.types import Command
-from core import sequence_graph, SequenceState
+from core import sequence_graph, SequenceState, main_graph, MainState
 
 router = APIRouter()
 
@@ -16,6 +16,40 @@ async def websocket_endpoint(websocket: WebSocket, thread_id: int):
         )
         while True:
             async for chunk in sequence_graph.astream(
+                input=input, config=config, stream_mode="updates"
+            ):
+                for node_id, value in chunk.items():
+                    if node_id == "__interrupt__":
+                        question = value[0].value.get(
+                            "question", "Human input required"
+                        )
+                        await websocket.send_text(f"[HUMAN_NEEDED] {question}")
+                        response = await websocket.receive_text()
+                        command = Command(resume=response)
+                        resumed_output = await sequence_graph.ainvoke(
+                            command, config=config
+                        )
+                        await websocket.send_text(
+                            f"[RESUMED_OUTPUT] {str(resumed_output)}"
+                        )
+                    else:
+                        await websocket.send_text(f"[STEP] {node_id}: {value}")
+    except Exception as e:
+        await websocket.send_text(f"[ERROR] {str(e)}")
+    finally:
+        await websocket.close()
+
+
+@router.websocket("/ws-chatbot/{thread_id}")
+async def websocket_endpoint(websocket: WebSocket, thread_id: int):
+    await websocket.accept()
+    try:
+        input = MainState()
+        config = RunnableConfig(
+            recursion_limit=150, configurable={"thread_id": thread_id}
+        )
+        while True:
+            async for chunk in main_graph.astream(
                 input=input, config=config, stream_mode="updates"
             ):
                 for node_id, value in chunk.items():
