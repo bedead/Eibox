e6de5b8fbc09d68d200b944b60c5398aadcb7eae
Satyam Mishra <satyammishra9050@gmail.com>
2025-07-02 14:58:06 +0530

    chore(core): remove imports from __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,7 @@
 from core.agents.sequence_graph import sequence_graph
 from core.agents.sequence_graph import SequenceState
-from core.agents.main_graph import MainState
 
-__all__ = ["sequence_graph", "SequenceState", "MainState"]
+__all__ = [
+    "sequence_graph",
+    "SequenceState",
+]
--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from core.utils.utils import display_graph
 from langgraph.config import RunnableConfig
 
 # display_graph(
-#     main_graph,
+#     graph,
 #     use_mermaid=True,
 #     use_api=True,
 # )
 
-# mainState = MainState()
-config = RunnableConfig(configurable={"thread_id": 1})
+
+# # mainState = MainState()
+# config = RunnableConfig(configurable={"thread_id": 1})
 
 
-while True:
-    user_i = input("You : ")
-    if user_i in ["exit", "close"]:
-        break
-    chunk = graph.stream(
-        input={"messages": {"role": "user", "content": user_i}},
-        config=config,
-        stream_mode="values",
-    )
-    for event in chunk:
-        if "messages" in event:
-            event['messages'][-1].pretty_print()
-        # print(node_id)
-        # not usefull as for now
-        # if node_id == "__interrupt__":
-        #     # question = value[0].value.get("question", "Human input required")
-        #     command = Command(resume={"data": input("You : ")})
-        #     resumed_output = graph.stream(command, config=config)
+# while True:
+#     user_i = input("You : ")
+#     if user_i in ["exit", "close"]:
+#         break
+#     chunk = graph.stream(
+#         input={"messages": {"role": "user", "content": user_i}},
+#         config=config,
+#         stream_mode="values",
+#     )
+#     for event in chunk:
+#         if "messages" in event:
+#             event['messages'][-1].pretty_print()
+#         # print(node_id)
+#         # not usefull as for now
+#         # if node_id == "__interrupt__":
+#         #     # question = value[0].value.get("question", "Human input required")
+#         #     command = Command(resume={"data": input("You : ")})
+#         #     resumed_output = graph.stream(command, config=config)
