8c5e6fe412f69e81eebc931d49399757fe50b4a5
Satyam Mishra <satyammishra9050@gmail.com>
2025-07-03 11:33:46 +0530

    Revert "chore(core): remove imports from __init__.py"
    This reverts commit e6de5b8fbc09d68d200b944b60c5398aadcb7eae.

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,5 @@
 from core.agents.sequence_graph import sequence_graph
 from core.agents.sequence_graph import SequenceState
+from core.agents.main_graph import MainState
 
-__all__ = [
-    "sequence_graph",
-    "SequenceState",
-]
+__all__ = ["sequence_graph", "SequenceState", "MainState"]
--- main.py
+++ main.py
@@ -6,44 +6,30 @@ from langgraph.config import RunnableConfig
 
 
 # display_graph(
-#     graph,
+#     main_graph,
 #     use_mermaid=True,
 #     use_api=True,
 # )
 
-
-mainState = SharedState()
+# mainState = MainState()
 config = RunnableConfig(configurable={"thread_id": 1})
 
-# if "messages" in event:
-#     event["messages"][-1].pretty_print()
 
-
-async def call():
-    while True:
-        user_i = input("You : ")
-        if user_i in ["exit", "close"]:
-            break
-        async for chunk in graph.astream(
-            input={"messages": {"role": "user", "content": user_i}},
-            config=config,
-            stream_mode="updates",
-            subgraphs=False,
-        ):
-            for node_id, value in chunk.items():
-                # print(node_id)
-                # not usefull as for now
-                if node_id == "__interrupt__":
-                    question = value[0].value.get("question")
-                    print(f"Interrupt Question : {question}")
-                    command = Command(resume={"data": input("You : ")})
-                    resumed_output = graph.astream(command, config=config)
-
-                    print(resumed_output)
-                else:
-                    print(f"STEP {node_id} : {value}")
-
-
-import asyncio
-
-asyncio.run(call())
+while True:
+    user_i = input("You : ")
+    if user_i in ["exit", "close"]:
+        break
+    chunk = graph.stream(
+        input={"messages": {"role": "user", "content": user_i}},
+        config=config,
+        stream_mode="values",
+    )
+    for event in chunk:
+        if "messages" in event:
+            event['messages'][-1].pretty_print()
+        # print(node_id)
+        # not usefull as for now
+        # if node_id == "__interrupt__":
+        #     # question = value[0].value.get("question", "Human input required")
+        #     command = Command(resume={"data": input("You : ")})
+        #     resumed_output = graph.stream(command, config=config)
