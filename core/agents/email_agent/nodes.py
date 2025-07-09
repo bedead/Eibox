b2ecfa3d409f6a488beff724cc8a9e8fb776407f
Satyam Mishra <satyammishra9050@gmail.com>
2025-07-09 22:21:30 +0530

    refactor(email_agent): remove unused response handling functions and update mail summary storage

--- core/agents/email_agent/nodes.py
+++ core/agents/email_agent/nodes.py
@@ -5,7 +5,6 @@ from core.utils.prompts import (
     IS_RESPONSE_NEEDED_PROMPT,
     MAIL_RESPONSE_FORMAT_PROMPT,
     GENERATE_MAIL_RESPONSE_SUGGESTION_PROMPT,
-    EDIT_SUGGESTED_RESPONSE_PROMPT,
 )
 from core.agents.email_agent.states import EmailState
 from langgraph.graph import END
@@ -86,7 +85,7 @@ def summarize_email(state: EmailState):
             HumanMessage(content=f"{email_data}"),
         ]
         summary = llm_model.invoke(messages).content.lower().strip()
-        store.put(namespace_for_memory, "mail_summary", random.randint(1, 10))
+        store.put(namespace_for_memory, "mail_summary", summary)
 
         return {"email_summary": summary}
 
@@ -142,113 +141,3 @@ def generate_draft_response(state: EmailState):
         store.put(namespace_for_memory, "draft_response", draft_response)
 
         return {"response_email_draft": draft_response}
-
-
-def get_response_approval(state: EmailState):
-    """
-    Get the response approval from the user.
-    """
-    # Simulate user approval for the response
-    print("Draft response:")
-    print(state["response_email_draft"])
-    is_approved = interrupt(
-        {"question": "Do you approve the draft response to be sent? (y/n):"}
-    )
-    if is_approved == "y":
-        user_approval = True
-    elif is_approved == "n":
-        user_approval = False
-    else:
-        print("Invalid input. Please enter 'y' or 'n'.")
-        return
-    return {"response_approved": user_approval}
-
-
-def get_draft_edit_mode(state: EmailState):
-    """
-    Get the draft edit mode from the user.
-    This is a function which lets users choose the edit mode of draft (manual/auto).
-    """
-    # Simulate user selecting the draft edit mode
-    edit_mode = interrupt(
-        {
-            "question": "How do you want to edit the draft? (manual: 0/auto: 1/ no response: 2):"
-        }
-    )
-    if edit_mode == 0:
-        return {"draft_manual_edit_mode": 0}
-    elif edit_mode == 1:
-        return {"draft_manual_edit_mode": 1}
-    elif edit_mode == 2:
-        return {"draft_manual_edit_mode": 2}
-    else:
-        print("Invalid input. Please enter 0 or 1 or 2.")
-        return
-
-
-def get_edited_response(state: EmailState):
-    """
-    Get the edited response from the user.
-    """
-    # Simulate user editing the response
-    manual_edited_draft = interrupt({"question": "Please edit the response:"})
-    if manual_edited_draft:
-        return {"response_email_draft": manual_edited_draft}
-    return
-
-
-def auto_edit_response(state: EmailState):
-    """
-    Auto edit the response for the email using the AI toolkit (LLM) by giving customization instruction.
-    """
-    if not state["email"]:
-        return
-    if (
-        state["is_mail_important"]
-        and state["is_response_needed"]
-        and state["response_email_draft"] != None
-        and state["draft_manual_edit_mode"] == 1
-    ):
-        customization_instruction = interrupt(
-            {"question": "Please provide customization instruction for the response:"}
-        )
-        email_data = state["email"]
-        messages = [
-            SystemMessage(content=EDIT_SUGGESTED_RESPONSE_PROMPT),
-            HumanMessage(content=f"Mail data :{email_data}"),
-            HumanMessage(content=f"Draft mail :{state['response_email_draft']}"),
-            HumanMessage(
-                content=f"Customization instruction :{customization_instruction}"
-            ),
-            HumanMessage(content=f"Mail style :{state['response_format']}"),
-        ]
-        edited_response_text = llm_model.invoke(messages).content.lower().strip()
-        return {"response_edited": edited_response_text}
-
-
-def send_email_response(state: EmailState):
-    """
-    Send the response for the email using the Gmail toolkit.
-    """
-    if not state["email"]:
-        return
-    if (
-        state["is_mail_important"]
-        and state["response_approved"]
-        and state["is_response_needed"]
-        and state["response_email_draft"] != None
-    ):
-        email_data = state["email"]
-        response_text = (
-            state["response_edited"]
-            if state["response_edited"]
-            else state["response_email_draft"]
-        )
-        # status = gmail_tool.send_mawil(
-        #     to=email_data["sender"],
-        #     subject=email_data["subject"],
-        #     body=response_text,
-        # )
-        # dummy testing mechanism
-        status = {"success": True}
-        return {"response_sent": status["success"]}
